a230ff9cc844cc1bfa4930ce1cce10eb8cf78acb
PaulThePainting <123056466+PaulThePainting@users.noreply.github.com>
2023-04-18 10:45:16 +0300

    Wordle game

--- WordleGame/src/Wordle.java
+++ WordleGame/src/Wordle.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Wordle {
     public static void main(String[] args) {
         WordleGame game = new WordleGame();
         Scanner scanner = new Scanner(System.in);
